65a9dd131e10d0747bf27d2754f44a27d4554043
Haroon khan <haroonkhan9426@gmail.com>
2019-05-02 17:42:23 +0500

    Basic Login Screen UI plus backend implementation done

--- IOS-Practice-Objective-C/ViewController.m
+++ IOS-Practice-Objective-C/ViewController.m
@@ -10,14 +10,39 @@
 
 @interface ViewController ()
 
+@property NSString *userName;
+@property NSString *password;
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *userPasswordTextField;
+@property (weak, nonatomic) IBOutlet UILabel *errorMessageLabel;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.userName = @"haroonkhan";
+    self.password = @"password";
+    
 }
+
+- (IBAction)loginButtonPressed:(id)sender {
+    if ([self.userName isEqualToString: self.userNameTextField.text] &&
+        [self.password isEqualToString:self.userPasswordTextField.text])
+    {
+        self.errorMessageLabel.text = @"Login Successfully";
+    }else{
+        self.errorMessageLabel.text = @"Login Failed";
+    }
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
 
 
 @end
